07b478ecf700eecc8f1817eae402a7fc6b572a3b
Vojtěch Šamla <auzkok@seznam.cz>
2025-08-05 21:03:53 +0200

    Initial prototype

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+function calculateIndex(str) {
+    str = str.toLowerCase();
+    var r = parseInt(str.substring(0,2), 16);
+    var g = parseInt(str.substring(2,4), 16);
+    var b = parseInt(str.substring(4), 16);
+    var a = 255;
+    return (3*r + 5*g + 7*b + 11*a) % 64;
+}
+
+window.onload = function() {
+    var inputs = document.querySelectorAll('.row > input');
+    inputs.forEach(function(input) {
+        input.addEventListener('input', function(e) {
+            if (this.checkValidity()) {
+                document.querySelector('#cache :nth-child(' + calculateIndex(this.value) +')').style.backgroundColor = '#' + this.value;
+                this.dataset.valid = this.value;
+            } else {
+                if (this.dataset.valid != "") {
+                    document.querySelector('#cache :nth-child(' + calculateIndex(this.value) +')').style.backgroundColor = '';
+                    this.dataset.valid = '';
+                }
+            }
+        });
+    })
+};
