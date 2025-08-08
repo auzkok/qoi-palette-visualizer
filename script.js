52204211bbb7ca6befa20ab6ac210aeab059fc73
Vojtěch Šamla <auzkok@seznam.cz>
2025-08-08 19:26:17 +0200

    Keep info about all colors for one space

--- script.js
+++ script.js
@@ -9,18 +9,37 @@ function calculateIndex(str) {
     return (3*r + 5*g + 7*b + 11*a) % 64;
 }
 
+function isValidColor(input) {
+    return input.value != '' && input.checkValidity();
+}
+
+var cacheColors = Array(64).fill([]);
+
 window.onload = function() {
     var inputs = document.querySelectorAll('.row > input');
     inputs.forEach(function(input) {
         input.addEventListener('input', function(e) {
-            if (this.checkValidity()) {
-                document.querySelector('#cache :nth-child(' + calculateIndex(this.value) +')').style.backgroundColor = '#' + this.value;
+            if (isValidColor(this)) {
+                var index = calculateIndex(this.value);
+                document.querySelector('#cache :nth-child(' + index +')').style.backgroundColor = '#' + this.value;
                 this.dataset.valid = this.value;
+                cacheColors[index].push(this.value);
             } else {
                 if (this.dataset.valid != "") {
-                    document.querySelector('#cache :nth-child(' + calculateIndex(this.dataset.valid) +')').style.backgroundColor = '';
+                    var index = calculateIndex(this.dataset.valid);
+                    document.querySelector('#cache :nth-child(' + index +')').style.backgroundColor = '';
+                    var colorPos = cacheColors[index].indexOf(this.dataset.valid);
+                    if (colorPos != -1) {
+                        cacheColors[index].splice(colorPos, 1);
+                        if (cacheColors[index].length > 0) {
+                            document.querySelector('#cache :nth-child(' + index +')').style.backgroundColor = '#' + cacheColors[index][cacheColors[index].length - 1];
+                        }
+                    }
                     this.dataset.valid = '';
                 }
+            }
+            if (index != undefined) {
+                console.log(cacheColors[index].length);
             }
         });
     })
